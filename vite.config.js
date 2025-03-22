30b09b2d3d939a02c7af0612198ddc5b0115bf2c
Zoya Ahmed <icon54008@gmail.com>
2025-03-22 23:50:45 +0530

    Updated css and renamed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for GitHub Pages
+  base: '/todo-list/', // Use repo name as base path
   plugins: [react()],
 });
